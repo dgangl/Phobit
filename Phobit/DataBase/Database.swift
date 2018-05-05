ff890ab3404c07f158734ebd227c4ae1edf68211
Paul <paulkrenn21@gmail.com>
2018-05-05 14:18:31 +0200

    Added the Login Method! Be careful not finished!

--- Phobit/DataBase/Database.swift
+++ Phobit/DataBase/Database.swift
@@ -10,10 +10,12 @@ import Foundation
 import Firebase
 class Database {
     let db:Firestore;
+    var goAhead: Bool;
     
     
     init() {
         db = Firestore.firestore();
+        goAhead = false;
     }
     
     func addNew(wholeString: String, companyName: String, Date: Date, Brutto: Double, Netto: Double, TenProzent: Double,ThirteenProzent : Double, NineteenProzent : Double, TwentyProzent: Double, Kontierung: String ) {
@@ -68,6 +70,46 @@ class Database {
         }
         
         return array;
+    }
+    
+    func checkUser(name: String, passwort: String) -> Bool {
+        goAhead = false;
+        var endThread = false;
+        
+        //Try to find the cirtain document
+        let docRef = db.collection("Nutzer").document(name);
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                //Document exists! Now we get the values of the Document!
+                let values = document.data()?.values;
+                
+                 //Run through all Datas in the Document
+                for val in values!{
+                   //Passwort is a String -> If String -> It should be the password!
+                    if let test = val as? String{
+                        //Is the given Password equals the correct Password?
+                        if(test.elementsEqual(passwort)){
+                            //THE PASSWORT MATCHED TO OUR USER!
+                            self.goAhead = true;
+                            endThread = true;
+                           
+                        }
+                    }
+                }
+                endThread = true;
+                
+            } else {
+                endThread = true;
+                //There is no User with this name
+                print("Document does not exist")
+            }
+        }
+        
+        
+        
+        
+        return goAhead;
     }
     
 }
