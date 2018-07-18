3dda7b8f2fa192612232d624a94e8a5302c2cb76
David Gangl <davidgangl9@icloud.com>
2018-07-18 17:57:57 +0200

    littleBugFix

--- Phobit/ViewController Files/AuswertungsView/ScanningViewController.swift
+++ Phobit/ViewController Files/AuswertungsView/ScanningViewController.swift
@@ -72,7 +72,7 @@ class ScanningViewController: UIViewController {
         super.viewDidLoad()
         
         
-        _ = NeuralNetDataGetter.init()
+       
         
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: Notification.Name.UIApplicationWillResignActive, object: nil)
